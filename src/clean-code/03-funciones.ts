80e6873ef6464014e3273bdd87d15f46eaf33870
Nicolas Sabbatini <nicolas.sabbatini.95@gmail.com>
2024-09-02 18:02:40 -0300

    ejercicio 4

--- src/clean-code/03-funciones.ts
+++ src/clean-code/03-funciones.ts
@@ -37,4 +37,21 @@
     console.log("Crear actor");
     return true;
   }
+
+  const getPayAmount = ({
+    isDead = false,
+    isSeparated = true,
+    isRetired = false,
+  }): number => {
+    if (isDead) {
+      return 1500;
+    }
+    if (isSeparated) {
+      return 2500;
+    }
+    if (isRetired) {
+      return 3000;
+    }
+    return 4000;
+  };
 })();
